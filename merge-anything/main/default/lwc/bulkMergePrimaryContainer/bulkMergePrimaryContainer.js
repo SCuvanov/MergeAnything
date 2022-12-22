202c9fae55b85ae904e6b36ab7de0e893c1d8f73
Sean Cuvanov <SCuvanov@gmail.com>
2022-12-22 16:36:26 -0500

    Fetching record and adding button visibility logic

--- merge-anything/main/default/lwc/bulkMergePrimaryContainer/bulkMergePrimaryContainer.js
+++ merge-anything/main/default/lwc/bulkMergePrimaryContainer/bulkMergePrimaryContainer.js
@@ -1,6 +1,7 @@
 import { LightningElement } from 'lwc';
 
-//TODO Stop using strings, define final variables or use labels
+//TODO: Stop using strings, define final variables or use labels
+//TODO: Maybe we shouldn't allow the add merge button unless we have a valid merge selected.
 
 export default class BulkMergePrimaryContainer extends LightningElement {
     value = 'merges';
@@ -10,5 +11,28 @@ export default class BulkMergePrimaryContainer extends LightningElement {
             { label: 'Merges', value: 'merges' },
             { label: 'Merge Items', value: 'merge_items' },
         ];
+    }
+
+    handleMergeOption(event) {
+        this.value = event.detail.value;
+
+        if(this.value === 'merges') {
+            this.template.querySelector('lightning-button[data-id=newMergeBtn]').hidden = false;
+            this.template.querySelector('lightning-button[data-id=addMergeItemBtn]').hidden = true;
+        } else {
+            this.template.querySelector('lightning-button[data-id=newMergeBtn]').hidden = true;
+            this.template.querySelector('lightning-button[data-id=addMergeItemBtn]').hidden = false;
+
+            //TODO: Add / Remove if there is a valid merge.
+            this.template.querySelector('lightning-button[data-id=addMergeItemBtn]').disabled = true; 
+        }
+    }
+
+    handleNewMerge(event) {
+        console.log('New Merge Clicked');
+    }
+
+    handleAddMergeItem(event) {
+        console.log('Add Merge Item Clicked');
     }
 }
--- merge-anything/main/default/lwc/bulkMergeSecondaryContainer/bulkMergeSecondaryContainer.js
+++ merge-anything/main/default/lwc/bulkMergeSecondaryContainer/bulkMergeSecondaryContainer.js
@@ -1,3 +1,20 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 
-export default class BulkMergeSecondaryContainer extends LightningElement {}
+import NAME_FIELD from '@salesforce/schema/Merge__c.Name';
+import STATUS_FIELD from '@salesforce/schema/Merge__c.Status__c';
+
+export default class BulkMergeSecondaryContainer extends LightningElement {
+    @api recordId = 'a013J000005AQLGQA4';
+
+    @wire(getRecord, { recordId: '$recordId', fields: [NAME_FIELD, STATUS_FIELD] })
+    merge;
+
+    get recordName() {
+        return getFieldValue(this.merge.data, NAME_FIELD);
+    }
+
+    get recordStatus(){
+        return getFieldValue(this.merge.data, STATUS_FIELD);
+    }
+}
