7fb0620e20796fd86778700ebf6003902873c630
Sean Cuvanov <SCuvanov@gmail.com>
2022-12-23 12:56:59 -0500

    Adding new components, restyling secondary container.

--- merge-anything/main/default/lwc/bulkMergePrimaryContainer/bulkMergePrimaryContainer.js
+++ merge-anything/main/default/lwc/bulkMergePrimaryContainer/bulkMergePrimaryContainer.js
@@ -2,30 +2,32 @@ import { LightningElement } from 'lwc';
 
 //TODO: Stop using strings, define final variables or use labels
 //TODO: Maybe we shouldn't allow the add merge button unless we have a valid merge selected.
+const MERGES = 'merges';
+const MERGE_ITEMS = 'merge_items';
+const NEW_MERGE_BTN_ID = 'lightning-button[data-id=newMergeBtn]';
+const ADD_MERGE_ITEM_BTN_ID = 'lightning-button[data-id=addMergeItemBtn]';
 
 export default class BulkMergePrimaryContainer extends LightningElement {
-    value = 'merges';
 
-    get options() {
+    //TODO: Should we get the newest Merge by CREATED DATE && Status? As a start point?
+    
+    _mergeOption;
+
+    constructor () {
+        super();
+        this._mergeOption = MERGES;
+    }
+
+    get _mergeOptions() {
         return [
-            { label: 'Merges', value: 'merges' },
-            { label: 'Merge Items', value: 'merge_items' },
+            { label: 'Merges', value: MERGES },
+            { label: 'Merge Items', value: MERGE_ITEMS },
         ];
     }
 
     handleMergeOption(event) {
-        this.value = event.detail.value;
-
-        if(this.value === 'merges') {
-            this.template.querySelector('lightning-button[data-id=newMergeBtn]').hidden = false;
-            this.template.querySelector('lightning-button[data-id=addMergeItemBtn]').hidden = true;
-        } else {
-            this.template.querySelector('lightning-button[data-id=newMergeBtn]').hidden = true;
-            this.template.querySelector('lightning-button[data-id=addMergeItemBtn]').hidden = false;
-
-            //TODO: Add / Remove if there is a valid merge.
-            this.template.querySelector('lightning-button[data-id=addMergeItemBtn]').disabled = true; 
-        }
+        this._mergeOption = event.detail.value;
+        this.toggleMergeOptionView(this._mergeOption);
     }
 
     handleNewMerge(event) {
@@ -34,5 +36,17 @@ export default class BulkMergePrimaryContainer extends LightningElement {
 
     handleAddMergeItem(event) {
         console.log('Add Merge Item Clicked');
+    }
+
+    toggleMergeOptionView(value) {
+        //TODO: Need to handle hiding/showing the listviews properly
+        
+        if(value === MERGES) {
+            this.template.querySelector(NEW_MERGE_BTN_ID).hidden = false;
+            this.template.querySelector(ADD_MERGE_ITEM_BTN_ID).hidden = true;
+        } else {
+            this.template.querySelector(NEW_MERGE_BTN_ID).hidden = true;
+            this.template.querySelector(ADD_MERGE_ITEM_BTN_ID).hidden = false;
+        }
     }
 }
