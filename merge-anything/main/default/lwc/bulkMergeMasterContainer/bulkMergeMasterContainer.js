add88758591501e58abf8ff4bbc2ad566fbdf480
Sean Cuvanov <SCuvanov@gmail.com>
2022-12-27 16:01:29 -0500

    Formatting files

--- merge-anything/main/default/lwc/bulkMergeMasterContainer/bulkMergeMasterContainer.js
+++ merge-anything/main/default/lwc/bulkMergeMasterContainer/bulkMergeMasterContainer.js
@@ -8,26 +8,26 @@ import NAME_FIELD from "@salesforce/schema/Merge__c.Name";
 import STATUS_FIELD from "@salesforce/schema/Merge__c.Status__c";
 
 export default class BulkMergeMasterContainer extends LightningElement {
-  _recordId;
-  _error;
+    _recordId;
+    _error;
 
-  @wire(getNewestMergeId, {})
-  wireNewestMergeId({ error, data }) {
-    if (data) {
-      this._recordId = data;
-      this._error = undefined;
-    } else if (error) {
-      this._error = error;
-      this._recordId = undefined;
-    } else {
-      this._error = undefined;
-      this._recordId = undefined;
+    @wire(getNewestMergeId, {})
+    wireNewestMergeId({ error, data }) {
+        if (data) {
+            this._recordId = data;
+            this._error = undefined;
+        } else if (error) {
+            this._error = error;
+            this._recordId = undefined;
+        } else {
+            this._error = undefined;
+            this._recordId = undefined;
+        }
     }
-  }
 
-  @wire(getRecord, {
-    recordId: "$_recordId",
-    fields: [ID_FIELD, NAME_FIELD, STATUS_FIELD],
-  })
-  _merge;
+    @wire(getRecord, {
+        recordId: "$_recordId",
+        fields: [ID_FIELD, NAME_FIELD, STATUS_FIELD],
+    })
+    _merge;
 }
--- merge-anything/main/default/lwc/bulkMergePrimaryContainer/bulkMergePrimaryContainer.js
+++ merge-anything/main/default/lwc/bulkMergePrimaryContainer/bulkMergePrimaryContainer.js
@@ -18,80 +18,80 @@ const NEW_MERGE_BTN_ID = "lightning-button[data-id=newMergeBtn]";
 const ADD_MERGE_ITEM_BTN_ID = "lightning-button[data-id=addMergeItemBtn]";
 
 export default class BulkMergePrimaryContainer extends LightningElement {
-  @api merge;
-  _merge;
-  _mergeOption;
-  _mergeView;
+    @api merge;
+    _merge;
+    _mergeOption;
+    _mergeView;
 
-  constructor() {
-    super();
-    this._mergeOption = MERGES;
-    this._mergeView = ALL_MERGES;
-  }
-
-  get merge() {
-    return this._merge;
-  }
-  set merge(value) {
-    this.setAttribute("merge", value);
-    this._merge = value;
-  }
-
-  get _mergeOptions() {
-    return [
-      { label: "Merges", value: MERGES },
-      { label: "Merge Items", value: MERGE_ITEMS },
-    ];
-  }
-
-  get _mergeViews() {
-    let mergeViews = [];
-    if (this._mergeOption === MERGES) {
-      mergeViews = [
-        { label: "All Merges", value: ALL_MERGES },
-        { label: "Pending Merges", value: PENDING_MERGES },
-        { label: "In Progress Merges", value: IN_PROGRESS_MERGES },
-        { label: "Completed Merges", value: COMPLETED_MERGES },
-        { label: "Failed Merges", value: FAILED_MERGES },
-      ];
-    } else if (this._mergeOption === MERGE_ITEMS) {
-      mergeViews = [
-        { label: "All Merge Items", value: ALL_MERGE_ITEMS },
-        { label: "Pending Merge Items", value: PENDING_MERGE_ITEMS },
-        { label: "In Progress Merge Items", value: IN_PROGRESS_MERGE_ITEMS },
-        { label: "Completed Merge Items", value: COMPLETED_MERGE_ITEMS },
-        { label: "Failed Merge Items", value: FAILED_MERGE_ITEMS },
-      ];
+    constructor() {
+        super();
+        this._mergeOption = MERGES;
+        this._mergeView = ALL_MERGES;
     }
-    return mergeViews;
-  }
 
-  handleMergeOption(event) {
-    this._mergeOption = event.detail.value;
-    this.toggleMergeOptionView(this._mergeOption);
-  }
-
-  handleNewMerge(event) {
-    console.log("New Merge Clicked");
-  }
-
-  handleAddMergeItem(event) {
-    console.log("Add Merge Item Clicked");
-  }
-
-  handleMergeView(event) {
-    this._mergeView = event.detail.value;
-  }
-
-  toggleMergeOptionView(value) {
-    if (value === MERGES) {
-      this.template.querySelector(NEW_MERGE_BTN_ID).hidden = false;
-      this.template.querySelector(ADD_MERGE_ITEM_BTN_ID).hidden = true;
-      this._mergeView = ALL_MERGES;
-    } else {
-      this.template.querySelector(NEW_MERGE_BTN_ID).hidden = true;
-      this.template.querySelector(ADD_MERGE_ITEM_BTN_ID).hidden = false;
-      this._mergeView = ALL_MERGE_ITEMS;
+    get merge() {
+        return this._merge;
     }
-  }
+    set merge(value) {
+        this.setAttribute("merge", value);
+        this._merge = value;
+    }
+
+    get _mergeOptions() {
+        return [
+            { label: "Merges", value: MERGES },
+            { label: "Merge Items", value: MERGE_ITEMS },
+        ];
+    }
+
+    get _mergeViews() {
+        let mergeViews = [];
+        if (this._mergeOption === MERGES) {
+            mergeViews = [
+                { label: "All Merges", value: ALL_MERGES },
+                { label: "Pending Merges", value: PENDING_MERGES },
+                { label: "In Progress Merges", value: IN_PROGRESS_MERGES },
+                { label: "Completed Merges", value: COMPLETED_MERGES },
+                { label: "Failed Merges", value: FAILED_MERGES },
+            ];
+        } else if (this._mergeOption === MERGE_ITEMS) {
+            mergeViews = [
+                { label: "All Merge Items", value: ALL_MERGE_ITEMS },
+                { label: "Pending Merge Items", value: PENDING_MERGE_ITEMS },
+                { label: "In Progress Merge Items", value: IN_PROGRESS_MERGE_ITEMS },
+                { label: "Completed Merge Items", value: COMPLETED_MERGE_ITEMS },
+                { label: "Failed Merge Items", value: FAILED_MERGE_ITEMS },
+            ];
+        }
+        return mergeViews;
+    }
+
+    handleMergeOption(event) {
+        this._mergeOption = event.detail.value;
+        this.toggleMergeOptionView(this._mergeOption);
+    }
+
+    handleNewMerge(event) {
+        console.log("New Merge Clicked");
+    }
+
+    handleAddMergeItem(event) {
+        console.log("Add Merge Item Clicked");
+    }
+
+    handleMergeView(event) {
+        this._mergeView = event.detail.value;
+    }
+
+    toggleMergeOptionView(value) {
+        if (value === MERGES) {
+            this.template.querySelector(NEW_MERGE_BTN_ID).hidden = false;
+            this.template.querySelector(ADD_MERGE_ITEM_BTN_ID).hidden = true;
+            this._mergeView = ALL_MERGES;
+        } else {
+            this.template.querySelector(NEW_MERGE_BTN_ID).hidden = true;
+            this.template.querySelector(ADD_MERGE_ITEM_BTN_ID).hidden = false;
+            this._mergeView = ALL_MERGE_ITEMS;
+        }
+    }
 }
